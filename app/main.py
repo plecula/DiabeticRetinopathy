0f875e4898bba4c9ce82a69eab990e0e4a74f79c
ula <plecula8@gmail.com>
2025-06-29 22:31:39 +0200

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload.html')
+def upload():
+    return render_template('upload.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
